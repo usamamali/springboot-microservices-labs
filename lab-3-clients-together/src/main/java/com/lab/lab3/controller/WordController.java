9091be702751e0c5912f37dca227848c486a665b
Usama M. Ali <eng.usama.muhammad@gmail.com>
2017-06-24 00:18:57 +0400

    merge all lab-3 clients in one app with profiles

---
+++ lab-3-clients-together/src/main/java/com/lab/lab3/controller/WordController.java
@@ -0,0 +1,20 @@
+package com.lab.lab3.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WordController {
+
+	@Value("${words}")
+	private String words;
+
+	@RequestMapping("/")
+	public @ResponseBody String getWord() {
+		String[] wordArray = words.split(",");
+		int i = (int) Math.round(Math.random() * (wordArray.length - 1));
+		return wordArray[i];
+	}
+}
