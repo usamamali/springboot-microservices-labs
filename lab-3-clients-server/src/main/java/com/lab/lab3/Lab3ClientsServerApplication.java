40ac5a246eca9a9e03c7c2c3d502eb6db7b7307e
Usama M. Ali <usama@usamamali-mac.local>
2017-06-23 15:37:48 +0400

    add configuration server for lab 3

---
+++ lab-3-clients-server/src/main/java/com/lab/lab3/Lab3ClientsServerApplication.java
@@ -0,0 +1,12 @@
+package com.lab.lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3ClientsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab3ClientsServerApplication.class, args);
+	}
+}
