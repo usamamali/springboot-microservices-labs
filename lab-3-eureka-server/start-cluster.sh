5583a5cb0a80f9537df3327f812d78ef2bace036
Usama M. Ali <eng.usama.muhammad@gmail.com>
2017-06-23 23:22:13 +0400

    enable eureka cluster

---
+++ lab-3-eureka-server/start-cluster.sh
@@ -0,0 +1,3 @@
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=primary" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=secondary" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=tertiary"
