ceab51a9bfd41f7752ac7e780a4f8234eb0b99b5
Usama M. Ali <usama@usamamali-mac.local>
2017-06-23 21:20:24 +0400

    add client server to eureka server

--- lab-3-clients-server/src/main/java/com/lab/lab3/Lab3ClientsServerApplication.java
+++ lab-3-clients-server/src/main/java/com/lab/lab3/Lab3ClientsServerApplication.java
@@ -2,9 +2,11 @@ package com.lab.lab3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Lab3ClientsServerApplication {
 
