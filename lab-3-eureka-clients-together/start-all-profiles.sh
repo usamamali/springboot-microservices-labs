454cfe407e62ba0ad4afcf72b39f2a038d9c9f74
Usama M. Ali <eng.usama.muhammad@gmail.com>
2017-12-27 10:27:37 +0400

    add bash files to run the projects

--- lab-3-eureka-clients-together/start-all-profiles.sh
+++ lab-3-eureka-clients-together/start-all-profiles.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+export M2_HOME=/Users/usama/tools/apache-maven-3.3.9/
+export PATH=$PATH:$M2_HOME/bin
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=subject" &
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=verb" &
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=article" &
--- lab-3-eureka-server/start-cluster.sh
+++ lab-3-eureka-server/start-cluster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+export M2_HOME=/Users/usama/tools/apache-maven-3.3.9/
+export PATH=$PATH:$M2_HOME/bin
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=primary" &
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=secondary" &
 mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=tertiary"
